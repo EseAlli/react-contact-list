b3645f1a9505d2fcc99e9f6c5d669eab005c243f
EseAlli <essealli.ea@gmail.com>
2021-12-12 10:35:17 +0100

    Project completed -- test pending

---
+++ tests/test_helper.js
@@ -0,0 +1,31 @@
+const Contact = require('../models/contact.model')
+
+
+const starterContact = [
+  {
+    first_name: 'Ese',
+    last_name: "Alli",
+    number: "08080816147",
+    email: "iamkuby@gmail.com",
+    created: new Date(),
+  }
+]
+
+const nonExistingId = async () => {
+  const contact = new Contact({ first_name: 'Fake', last_name:"Name", number: "0808089736829", created: new Date() })
+  await contact.save()
+  await contact.remove()
+
+  return contact._id.toString()
+}
+
+const contactsInDatabase = async () => {
+  const contacts = await Contact.find({})
+  return contacts.map(contact => contact.toJSON())
+}
+
+module.exports = {
+  starterContact,
+  nonExistingId,
+  contactsInDatabase,
+}
