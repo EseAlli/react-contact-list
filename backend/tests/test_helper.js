cb33cc750e7a568d18369e89730400f6d3c51ede
EseAlli <essealli.ea@gmail.com>
2021-12-13 09:31:29 +0100

    changes to test helper file

--- backend/tests/test_helper.js
+++ backend/tests/test_helper.js
@@ -1,16 +1,6 @@
 const Contact = require('../models/contact.model')
 
 
-const starterContact = [
-  {
-    first_name: 'Ese',
-    last_name: "Alli",
-    number: "08080816147",
-    email: "iamkuby@gmail.com",
-    created: new Date(),
-  }
-]
-
 const nonExistingId = async () => {
   const contact = new Contact({ first_name: 'Fake', last_name:"Name", number: "0808089736829", email:"fakeemail@gmail.com", created: new Date() })
   await contact.save()
