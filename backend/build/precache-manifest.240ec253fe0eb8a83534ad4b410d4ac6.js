98ab21d9b5fe997e4697d1b009f0b6866f6bc652
EseAlli <essealli.ea@gmail.com>
2021-12-13 12:29:40 +0100

    backend build

---
+++ backend/build/precache-manifest.240ec253fe0eb8a83534ad4b410d4ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9c0a7e0049deae98e9eff28df23168",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d320a0e75dd64d72e2fc",
+    "url": "/static/css/main.6dc0381c.chunk.css"
+  },
+  {
+    "revision": "ea313ffc35f082968eae",
+    "url": "/static/js/2.e65574ac.chunk.js"
+  },
+  {
+    "revision": "0e2038a396be73f0deed7939a8c160de",
+    "url": "/static/js/2.e65574ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d320a0e75dd64d72e2fc",
+    "url": "/static/js/main.a69f5c1b.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
